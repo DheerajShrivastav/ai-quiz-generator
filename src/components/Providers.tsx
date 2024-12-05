316cf93e8990d0549b4fe67d047e3ac5bdab3735
Dheeraj Shrivastav <dheerajshrivastav98@gmail.com>
2024-12-05 09:30:40 +0530

    Refactor LoadingQuestions and Providers components for improved readability and consistency; streamline imports and loading text logic

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,11 +3,7 @@ import React from 'react'
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient()
 
 const Providers = ({ children }: ThemeProviderProps) => {
